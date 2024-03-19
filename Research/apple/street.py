0a094ae71d36817f99051494e6da18f1799654de
njhayes2 <njhayes2@illinois.edu>
2024-03-18 22:02:00 -0500

    implemented collsions

--- Research/apple/street.py
+++ Research/apple/street.py
@@ -12,7 +12,7 @@ imp = pygame.image.load("C:\\Users\\noahj\\OneDrive\\Pictures\\little guy.png").
 imp_1 = pygame.image.load("C:\\Users\\noahj\\OneDrive\\Pictures\\leaf.png").convert()
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-rect_1 = pygame.Rect(random.randint(0,500), random.randint(0,500), 25, 25)
+rect_1 = pygame.Rect(random.randint(0,500), random.randint(0,500), 60, 60)
 
 energy = 1.0
 health = 1.0
@@ -27,11 +27,35 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
 
+    player_rect = pygame.Rect(player_pos, imp.get_size())
     screen.blit(imp, (player_pos))
     screen.blit(imp_1, (rect_1))
     pygame.draw.rect(screen, "green", pygame.Rect(0,0, 80 * energy, 20))
     pygame.draw.rect(screen, "red", pygame.Rect(80 * energy,0, 80 * (1 - energy), 20))
     pygame.draw.rect(screen, "red", pygame.Rect(1200, 0, 80 * health, 20))
+    up = True
+    down = True
+    left = True
+    right = True
+    if player_rect.colliderect(rect_1)and health >= 0:
+        health -= dt/5
+        if (health < 0):
+            health = 0
+        if player_pos.x < rect_1.x and player_pos.y < rect_1.y:
+            down = False
+            right = False
+        if player_pos.x >= rect_1.x and player_pos.y < rect_1.y:
+            down = False
+            left = False
+        if player_pos.x < rect_1.x and player_pos.y >= rect_1.y:
+            right = False
+            up = False
+        if player_pos.x >= rect_1.x and player_pos.y >= rect_1.y:
+            left = False
+            up = False
+    if not player_rect.colliderect(rect_1) and health < 1:
+        health += dt/30
+
         
 
     keys = pygame.key.get_pressed()
@@ -46,13 +70,13 @@ while running:
         screen.fill("green")
     if keys[pygame.K_p]:
         screen.fill("purple")
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w] and up:
         player_pos.y -= speed * dt
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] and down:
         player_pos.y += speed * dt
-    if keys[pygame.K_a]:
+    if keys[pygame.K_a] and left:
         player_pos.x -= speed * dt
-    if keys[pygame.K_d]:
+    if keys[pygame.K_d] and right:
         player_pos.x += speed * dt
 
     # flip() the display to put your work on screen
